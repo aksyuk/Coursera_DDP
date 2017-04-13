b4b3d60a98b49ca43fc5829f08a609cff1eed45b
Unknown <s.a.aksuk@gmail.com>
2017-04-14 00:56:28 +0300

    Week 4 added
    Shiny app for course project

---
+++ week_4/server.R
@@ -0,0 +1,75 @@
+
+library('data.table')
+library('googleVis')
+library('WDI')
+library('reldist')
+
+# список показателей для загрузки
+indexes.list <- data.table(name = c('GDP per capita (current US$)',
+                                    'GDP per capita (constant 2000 US$)',
+                                    'GDP per capita, PPP (current international $)',
+                                    'GDP per capita, PPP (constant 2005 international $)'),
+                           indicator = c('NY.GDP.PCAP.CD',
+                                         'NY.GDP.PCAP.KD',
+                                         'NY.GDP.PCAP.PP.CD',
+                                         'NY.GDP.PCAP.PP.KD'),
+                           stringsAsFactors = F)
+
+# серверная часть приложения shiny
+shinyServer(function(input, output) {
+    
+    # реагирующая таблица с данными
+    DT <-  reactive({
+        na.omit(data.frame(WDI(indicator = indexes.list[name == input$index.name, indicator], 
+                               start = input$year, end = input$year))[48:264, ])
+    })
+
+    # output$sp.text <- renderText({
+    #     paste0('Selected indicator code: ',
+    #           indexes.list[name == input$index.name, name],
+    #           ' Year: ', input$year)
+    #     }) 
+    
+    # таблица данных в отчёте
+    output$gvis.widget <- renderGvis({
+        
+        g.chart <- gvisGeoChart(data = DT(), 
+                                locationvar = 'iso2c', 
+                                colorvar = colnames(DT())[3], 
+                                options = list(width = 700, 
+                                               height = 400, 
+                                               dataMode = 'regions'))
+        # вставляем результат в html-документ
+        return(g.chart)
+    })
+    
+    # сводка по статистике
+    output$summary.header <- renderPrint({
+        cat('<h3>Summary statistics:</h3>')
+    })
+    output$summary <- renderPrint({
+        summary(DT()[, 3])
+    })
+    
+    # коэффициент Джинни
+    output$gini.text <- renderPrint({
+        cat('<h3><a href = "https://en.wikipedia.org/wiki/Gini_coefficient">Gini coefficient</a> for selected indicator: ',
+            '<b>', round(gini(DT()[, 3]), 2), '</b></h3>')
+    })
+    
+    # Top 5
+    output$top.5.header <- renderPrint({
+        cat('<h3>Top 5:</h3>')
+    })
+    output$top.5 <- renderPrint({
+        DT()[order(-DT()[, 3]), 2:3][1:5, ]
+    })
+    
+    # Low 5
+    output$low.5.header <- renderPrint({
+        cat('<h3>Low 5:</h3>')
+    })
+    output$low.5 <- renderPrint({
+        DT()[order(DT()[, 3]), 2:3][1:5, ]
+    })
+})
---
+++ week_4/ui.R
@@ -0,0 +1,51 @@
+
+library('shiny')
+
+# список показателей для загрузки
+indexes.list <- data.table(name = c('GDP per capita (current US$)',
+                                    'GDP per capita (constant 2000 US$)',
+                                    'GDP per capita, PPP (current international $)',
+                                    'GDP per capita, PPP (constant 2005 international $)'),
+                           indicator = c('NY.GDP.PCAP.CD',
+                                         'NY.GDP.PCAP.KD',
+                                         'NY.GDP.PCAP.PP.CD',
+                                         'NY.GDP.PCAP.PP.KD'),
+                           stringsAsFactors = F)
+
+# размещение всех объектов на странице
+shinyUI(
+    # создать страницу с боковой панелью и главной областью для отчётов
+    pageWithSidebar(
+        # название приложения:
+        headerPanel('GDP per capita on Map'),
+        # боковая панель
+        sidebarPanel(
+            # слайдер по годам
+            sliderInput('year',
+                        'Select year', 
+                        min = 2000, max = 2015, value = 2015, format = '####'),
+            # выпадающее меню: показатели для изображения
+            radioButtons('index.name',   # связанная переменная
+                        'Select indicator to plot:', indexes.list[, name], 
+                        selected = indexes.list[1, name])
+        ),
+        # главная область
+        mainPanel(
+            # # текстовый объект для отображения выбранных параметров отбора
+            # textOutput('sp.text'),
+            # карта
+            htmlOutput('gvis.widget'),
+            # описательные статистики
+            htmlOutput('summary.header'),
+            verbatimTextOutput('summary'),
+            # текстовый объект для отображения коэффициента Джинни
+            htmlOutput('gini.text'),
+            # текстовый объект для отображения первой пятёрки
+            htmlOutput('top.5.header'),
+            verbatimTextOutput('top.5'),
+            # текстовый объект для отображения последней пятёрки
+            htmlOutput('low.5.header'),
+            verbatimTextOutput('low.5')
+            )
+        )
+    )
